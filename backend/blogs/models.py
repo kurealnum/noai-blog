bfb9cb18727efc9aed6bbead095784a9f122d81f
kurealnum <oscar.gaske.cs@gmail.com>
2024-08-17 19:13:31 -0400

    Finishes issue #2

--- backend/blogs/models.py
+++ backend/blogs/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=10000)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+class PostReaction(models.Model):
+    user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(to=BlogPost, on_delete=models.SET_NULL, null=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(to=BlogPost, on_delete=models.SET_NULL, null=True)
+    content = models.TextField(max_length=10000)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+class CommentReaction(models.Model):
+    user = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(to=BlogPost, on_delete=models.SET_NULL, null=True)
+
+
+class ReplyTo(models.Model):
+    comment = models.ForeignKey(to=Comment, on_delete=models.CASCADE)
+    reply = models.ForeignKey(to=Comment, on_delete=models.CASCADE)
