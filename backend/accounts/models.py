daa35587781e797bde3a56839586657ed45b8113
kurealnum <oscar.gaske.cs@gmail.com>
2024-08-17 17:26:48 -0400

    Added a new app, updated diagram, made change to gitignore

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(max_length=200)
+    first_name = models.CharField(max_length=50, unique=False)
+    last_name = models.CharField(max_length=50, unique=False)
+    about_me = models.TextField(max_length=250)
+    password = models.CharField(max_length=100, unique=False)
