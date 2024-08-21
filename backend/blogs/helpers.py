13daf08fe323d458bd148ea20724aeb7ea259cff
kurealnum <oscar.gaske.cs@gmail.com>
2024-08-20 23:34:52 -0400

    Fixes issue #8

--- backend/blogs/helpers.py
+++ backend/blogs/helpers.py
@@ -2,7 +2,6 @@ from django.db import connection
 
 
 def get_comment_replies(comment_id):
-    # FIXME: still need to fix this query to join everything
     query = f"""
         WITH RECURSIVE comments AS (
                 SELECT reply_id 
@@ -11,9 +10,10 @@ def get_comment_replies(comment_id):
             UNION ALL
                 SELECT comment_id
                 FROM blogs_replyto AS br
-                JOIN blogs_comment bc ON bc.id=br.comment_id
         )
-        SELECT * FROM comments;
+        SELECT * 
+        FROM comments AS c
+        JOIN blogs_comment bc ON bc.id = c.reply_id;
         """
 
     with connection.cursor() as cursor:
