c7840f5a16375341dbdd93323ad54bbc291cde1e
kurealnum <oscar.gaske.cs@gmail.com>
2024-09-13 22:20:42 -0400

    Basic tests written

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Source code for the NoAI blogging site
 
+## Testing
+
+The backend is tested with `unittest`. 99% of the backend tests are unit tests, as the name suggets. Integration testing and end-to-end testing isn't really valuable for the backend, and the testing files aren't really big enough to warrant smoke testing.
+
+The frontend is tested with React Testing Library and Vitest. React Testing Library takes care of integration testing (to be frank, it somewhat does both), and Vitest takes care of unit testing. There is currently no End-to-End testing.
+
+The "middleware" (Nginx and Docker) is not tested.
+
 ## Technologies used/that I would like to use
 
 Technologies that are currently in use:
--- frontend/tests/integrationtests/components/ErrorMessage.test.jsx
+++ frontend/tests/integrationtests/components/ErrorMessage.test.jsx
@@ -1,15 +1,35 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ErrorMessage from "../../../src/components/ErrorMessage";
-import { expect, test } from "vitest";
+import { describe, expect, it } from "vitest";
 
-test("does error render with isError = false", () => {
-  const rendered = render(
-    <ErrorMessage
-      isError={false}
-      message="Error message"
-      altMessage="No error message"
-    />,
-  );
-  expect(rendered.getByRole("paragraph")).toHaveTextContent("No error message");
+describe("ErrorMessage", () => {
+  it("does altmessage render with isError = false", () => {
+    const rendered = render(
+      <ErrorMessage
+        isError={false}
+        message="Error message"
+        altMessage="No error message"
+      />,
+    );
+    expect(rendered.getByRole("paragraph")).toHaveTextContent(
+      "No error message",
+    );
+  });
+  it("does message render with isError = true", () => {
+    const rendered = render(
+      <ErrorMessage
+        isError={true}
+        message="Error message"
+        altMessage="No error message"
+      />,
+    );
+    expect(rendered.getByRole("paragraph")).toHaveTextContent("Error message");
+  });
+  it("does component function when altmessage is null", () => {
+    const rendered = render(
+      <ErrorMessage isError={false} message="Error message" />,
+    );
+    expect(rendered.queryByRole("paragraph")).toBeNull();
+  });
 });
