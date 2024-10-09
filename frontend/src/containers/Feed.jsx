7c15ca62586fc36cf2e49ef453cebc03e31bf07b
kurealnum <oscar.gaske.cs@gmail.com>
2024-10-08 23:14:28 -0400

    Still getting yelled at about key issues, but closes #61

--- frontend/src/containers/Feed.jsx
+++ frontend/src/containers/Feed.jsx
@@ -5,6 +5,8 @@ import { CircularProgress } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 
 function Feed() {
+  // the amount of advertisements to show every x posts: advertise ratio of 5 = 1 ad every 5 posts
+  const advertiseRatio = 7;
   const [page, setPage] = useState(1);
   const { data, isLoading } = useQuery({
     queryKey: ["getFeed", page],
@@ -39,13 +41,28 @@ function Feed() {
   return (
     <div id="feed">
       {data.map((content, index) => (
-        <BlogPostThumbnail
-          key={index}
-          title={content.title}
-          username={content.user.username}
-          createdDate={content.created_date}
-          content={content.content}
-        />
+        <>
+          <BlogPostThumbnail
+            key={index}
+            title={content.title}
+            username={content.user.username}
+            createdDate={content.created_date}
+            content={content.content}
+          />
+          {(index + 1) % advertiseRatio == 0 ? (
+            <div
+              className="feed-advertisement"
+              key={"feed-advertisement" + index}
+            >
+              <a
+                href="mailto: thenoaiblog@gmail.com"
+                key={"feed-advertisement" + index}
+              >
+                Contact us about advertisements!
+              </a>
+            </div>
+          ) : null}
+        </>
       ))}
       <Paginator formSubmitHelper={formSubmitHelper} page={page} />
     </div>
