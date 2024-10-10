429db52cd14fa1c3aa226e37891d837c4628df61
kurealnum <oscar.gaske.cs@gmail.com>
2024-10-10 12:59:10 -0400

    Fixed a silly little issue

--- frontend/src/containers/NavBar.jsx
+++ frontend/src/containers/NavBar.jsx
@@ -1,5 +1,5 @@
 import "../styles/NavBar.css";
-import { useLoaderData } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { doesPathExist } from "../features/helpers";
 import Logo from "/public/shortlogo.svg";
@@ -21,40 +21,43 @@ function NavBar() {
   }, [userData]);
 
   return (
-    <nav>
-      <div className="nav-left">
-        <button className="nav-username-box">
-          {userData["username"] == null ? (
-            <a href="/login">Log in</a>
-          ) : (
-            <>
-              {exists ? (
-                <img id="pfp" src={userData["profile_picture"]}></img>
-              ) : null}
-              <ExpandMore />
-            </>
-          )}{" "}
-        </button>
-        <ul>
-          <li className="navbar-item">
-            <a href="/feed">
-              <LibraryBooksIcon />
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="/create-post">
-              <Add />
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="/notifications">
-              <Notifications />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <img id="logo" src={Logo} alt="Logo"></img>
-    </nav>
+    <>
+      <nav>
+        <div className="nav-left">
+          <button className="nav-username-box">
+            {userData["username"] == null ? (
+              <a href="/login">Log in</a>
+            ) : (
+              <>
+                {exists ? (
+                  <img id="pfp" src={userData["profile_picture"]}></img>
+                ) : null}
+                <ExpandMore />
+              </>
+            )}{" "}
+          </button>
+          <ul>
+            <li className="navbar-item">
+              <a href="/feed">
+                <LibraryBooksIcon />
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="/create-post">
+                <Add />
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="/notifications">
+                <Notifications />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <img id="logo" src={Logo} alt="Logo"></img>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
