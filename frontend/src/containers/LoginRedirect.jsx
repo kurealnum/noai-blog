5e0a94067311adae9aa8944f297b057de48a52db
kurealnum <oscar.gaske.cs@gmail.com>
2024-10-10 23:09:18 -0400

    Closes #73

--- frontend/src/containers/LoginRedirect.jsx
+++ frontend/src/containers/LoginRedirect.jsx
@@ -1,3 +1,4 @@
+/* v8 ignore start */
 // this is very hacky. doing this so isAuthenticated gets called again and navbar updates. see issue #52 for more information (specifically in commit history)
 import { Navigate } from "react-router-dom";
 
