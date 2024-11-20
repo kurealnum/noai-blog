baf31dda326b05b778f2dafb24443b4f324e5f9a
kurealnum <oscar.gaske.cs@gmail.com>
2024-11-20 16:25:40 -0500

    Hotfix

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -144,7 +144,7 @@ const router = createBrowserRouter([
       {
         path: "login",
         element: (
-          <Page title="Login" type="public-only">
+          <Page title="Login" type="public">
             <Login />
           </Page>
         ),
