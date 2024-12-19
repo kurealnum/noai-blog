1f3cd99547f694f43538ba1385a3dd345723f492
kurealnum <oscar.gaske.cs@gmail.com>
2024-12-19 14:50:46 -0500

    Previous commit was not a build. This is.

--- frontend/dist/assets/ErrorMessage-BRl7zG0-.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-YEr_JHM6.js";function i({isError:o,message:s,altMessage:r}){return o?n.jsx("p",{children:s}):r?n.jsx("p",{children:r}):null}export{i as E};
--- frontend/dist/assets/LoginRedirect-DWZ6pBPj.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as a}from"./index-YEr_JHM6.js";function r(){return t.jsx(a,{to:"/dashboard"})}export{r as default};
