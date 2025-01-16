3646690f29e82f5acef603c7202a13751e5e3a51
kurealnum <oscar.gaske.cs@gmail.com>
2025-01-16 13:45:07 -0500

    Added a CrosspostComment model

--- backend/crossposts/models.py
+++ backend/crossposts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 from accounts.models import CustomUser
+from blogs.models import get_sentinel_user
 
 
 class Crosspost(models.Model):
@@ -27,3 +28,29 @@ class CrosspostReaction(models.Model):
 
     class Meta:  # type: ignore
         unique_together = "user", "post"
+
+
+class CrosspostComment(models.Model):
+    user = models.ForeignKey(to=CustomUser, on_delete=models.SET(get_sentinel_user))
+    post = models.ForeignKey(
+        to=Crosspost, on_delete=models.CASCADE, null=True, blank=True
+    )
+    content = models.TextField(max_length=2000)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    is_read = models.BooleanField(default=False)
+    reply_to = models.ForeignKey(
+        "self", on_delete=models.DO_NOTHING, null=True, blank=True, related_name="reply"
+    )
+    flagged = models.BooleanField(default=False)
+
+    def delete(self, *args, **kwargs):  # type: ignore
+        self.content = "This comment was deleted"
+        self.is_read = True
+        self.user = get_sentinel_user()
+        self.save()
+        return
+
+    def toggle_flagged(self):
+        self.flagged = not self.flagged
+        self.save()
